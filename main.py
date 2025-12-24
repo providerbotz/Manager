96940613a84c9ca8c751cfdfa4dd5b1005177b16
˹  ᴘʀᴏᴠɪᴅᴇʀʙᴏᴛᴢ  ˼ <animeokids@gmail.com>
2025-12-24 15:36:13 +0530

    Start Ans. For Render

---
+++ main.py
@@ -0,0 +1,20 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+import logging
+from handlers import register_all_handlers
+from db import db
+
+logging.basicConfig(level=logging.INFO)
+
+app = Client(
+    "group_manger_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+register_all_handlers(app)
+
+print("Your Provider Bot Is Starting... ")
+
+app.run()
